df8d38694eb6857975b331d776e177a74fa3f6eb
AspirantDev <dormantdeveloper@gmail.com>
2022-10-10 16:22:03 +0100

    Add project files.

---
+++ TakingUserInput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TakingUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
